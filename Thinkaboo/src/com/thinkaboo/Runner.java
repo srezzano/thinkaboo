10b5cedd55284a77e36d78603640e2b6e781a151
Sebastian Rezzano <srezzano@gmail.com>
2013-08-01 23:01:42 -0300

    proyecto phonegap en blanco

---
+++ Thinkaboo/src/com/thinkaboo/Runner.java
@@ -0,0 +1,15 @@
+package com.thinkaboo;
+
+import org.apache.cordova.DroidGap;
+import android.os.Bundle;
+
+public class Runner extends DroidGap {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+
+
+}
